0387c60462b4af9fdbf5becb67fbc7e567fea51c
Oscar Polanco <opolanco@intellisys.com.do>
2021-02-07 23:57:08 -0400

    Create a Page component to use it on all pages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,9 @@
+import Page from '../components/Page';
+
 export default function IndexPage() {
   return (
-    <div>
+    <Page>
       <p>Hello</p>
-    </div>
+    </Page>
   );
 }
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,7 +1,9 @@
+import Page from '../components/Page';
+
 export default function SellPage() {
   return (
-    <div>
+    <Page>
       <p>Hello</p>
-    </div>
+    </Page>
   );
 }
