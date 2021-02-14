7b7d27b090f49940c2a409479fdb02f510320c2b
Oscar Polanco <opolanco@intellisys.com.do>
2021-02-14 00:05:47 -0400

    Use the nprogress library and add the Head component to _document

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,13 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+// TODO: swap with our own
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
